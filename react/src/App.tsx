449862061a3394a5f9250a1695080e31366756fc
Ahmed Al Jafarawy <prog.al.jafarawy@gmail.com>
2025-05-04 11:59:16 +0300

    convert top students into react

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.tsx";
-import Header from "./Components/Header/header.tsx";
-import Profile from "./Components/Profile/Profile.tsx";
-import Reports from "./Components/Report/Report .tsx";
-import Video from "./Components/Vid/Vid.tsx";
-import Works from "./Components/Works/Works.tsx";
-import About from "./Components/About/About.tsx";
-import TopStudents from "./Components/TopStudents/TopStudents.tsx";
-import Courses from "./Components/Courses/Courses.tsx";
-import Pricing from "./Components/Pricing/Pricing.tsx";
-import Blog from "./Components/Blog/Blog.tsx";
-import Contact from "./Components/Contact/Contact.tsx";
-import Login from "./Components/Login/Login.tsx";
-import SignUp from "./Components/SignUp/SignUp.tsx";
-import Footer from "./Components/Footer/Footer.tsx";
+import Header from "./Components/HTML COMPO/Header/header.tsx";
+import Profile from "./Components/HTML COMPO/Profile/Profile.tsx";
+import Reports from "./Components/HTML COMPO/Report/Report .tsx";
+import Video from "./Components/HTML COMPO/Vid/Vid.tsx";
+import Works from "./Components/HTML COMPO/Works/Works.tsx";
+import About from "./Components/HTML COMPO/About/About.tsx";
+import TopStudents from "./Components/HTML COMPO/TopStudents.tsx";
+import Courses from "./Components/HTML COMPO/Courses/Courses.tsx";
+import Pricing from "./Components/HTML COMPO/Pricing/Pricing.tsx";
+import Blog from "./Components/HTML COMPO/Blog/Blog.tsx";
+import Contact from "./Components/HTML COMPO/Contact/Contact.tsx";
+import Login from "./Components/HTML COMPO/Login/Login.tsx";
+import SignUp from "./Components/HTML COMPO/SignUp/SignUp.tsx";
+import Footer from "./Components/css/Footer/Footer.tsx";
 
 const App = () => {
   return (
---
+++ react/src/Components/HTML COMPO/TopStudents.tsx
@@ -0,0 +1,164 @@
+
+import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "../css/style.css";
+import "../css/main/top-stud.css";
+
+const TopStudents = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  return (
+    <>
+      
+      <section
+        className="hero-wrap hero-wrap-2"
+        style={{ backgroundImage: `url('/images/bg_1.jpg')` }}
+      >
+        <div className="overlay"></div>
+        <div className="container">
+          <div className="row no-gutters slider-text align-items-center justify-content-center">
+            <div className="col-md-9 ftco-animate text-center">
+              <h1 className="mb-2 bread">Top Students</h1>
+              <p className="breadcrumbs">
+                <span className="mr-2">
+                  <a href="/">
+                    Home <i className="ion-ios-arrow-forward"></i>
+                  </a>
+                </span>
+                <span>Top Students</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="container">
+        <div className="about-this-page">
+          <h2>What This Page Is About</h2>
+          <p className="about-top-students-page">
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+            Lorem ipsum dolor sit amet consectetur adipisicing eli
+          </p>
+        </div>
+
+        <div className="all-groups">
+          {/* الصفوف الثلاثة */}
+          {[
+            {
+              grade: "Three",
+              students: [
+                {
+                  name: "Ahmed Gensh",
+                  img: "./std-grad-3-1st.jpg",
+                  pos: "First",
+                  className: "postion-1",
+                },
+                {
+                  name: "Ibrahim Salama",
+                  img: "../images/std-grad-3-2nd.jpg",
+                  pos: "Second",
+                  className: "postion-2",
+                },
+                {
+                  name: "Habiba Mohamed",
+                  img: "std-grad-3-3rd.jpg",
+                  pos: "Third",
+                  className: "postion-3",
+                },
+              ],
+            },
+            {
+              grade: "Two",
+              students: [
+                {
+                  name: "Arwa Ramy",
+                  img: "std-grad-2-1st.jpg",
+                  pos: "First",
+                  className: "postion-1",
+                },
+                {
+                  name: "Mustafa Sayed",
+                  img: "std-grad-2-2nd.jpg",
+                  pos: "Second",
+                  className: "postion-2",
+                },
+                {
+                  name: "Alaa Samy",
+                  img: "std-grad-2-3rd.jpg",
+                  pos: "Third",
+                  className: "postion-3",
+                },
+              ],
+            },
+            {
+              grade: "One",
+              students: [
+                {
+                  name: "Ahmed Ashraf",
+                  img: "std-grad-1-1st.jpg",
+                  pos: "First",
+                  className: "postion-1",
+                },
+                {
+                  name: "Islam Sameh",
+                  img: "std-grad-1-2nd.jpg",
+                  pos: "Second",
+                  className: "postion-2",
+                },
+                {
+                  name: "Ragab Mahmoud",
+                  img: "std-grad-1-3rd.jpg",
+                  pos: "Third",
+                  className: "postion-3",
+                },
+              ],
+            },
+          ].map((group, index) => (
+            <div key={index} className={`secondary-grade-${3 - index}`}>
+              <h2 className="group-header">
+                Top Secondary {group.grade} Students
+              </h2>
+              <div className="same-grade">
+                {group.students.map((stud, i) => (
+                  <div key={i} className="stud-card">
+                    <img
+                      className="card-img"
+                      src={`/images/${stud.img}`}
+                      alt={stud.name}
+                    />
+                    <div className="card-info">
+                      <h3 className="card-h3">{stud.name}</h3>
+                      <div className={stud.className}>
+                        <span className="card-span">{stud.pos} </span>
+                        <i className="fa-solid fa-medal"></i>
+                      </div>
+                      <p className="card-para">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Repellat, quibusdam.
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TopStudents;
