8e882328cb8c606703f4d603ab9b8b48d32d90bf
Ahmed Al Jafarawy <prog.al.jafarawy@gmail.com>
2025-05-06 20:24:35 +0300

    top students

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home.tsx";
-import Header from "./Components/Header/header.tsx";
-import Profile from "./Components/Profile/Profile.tsx";
-import Reports from "./Components/Report/Report .tsx";
-import Video from "./Components/Vid/Vid.tsx";
-import Works from "./Components/Works/Works.tsx";
-import About from "./Components/About/About.tsx";
-import TopStudents from "./Components/TopStudents/TopStudents.tsx";
-import Courses from "./Components/Courses/Courses.tsx";
-import Pricing from "./Components/Pricing/Pricing.tsx";
-import Blog from "./Components/Blog/Blog.tsx";
-import Contact from "./Components/Contact/Contact.tsx";
-import Login from "./Components/Login/Login.tsx";
-import SignUp from "./Components/SignUp/SignUp.tsx";
-import Footer from "./Components/Footer/Footer.tsx";
+import Header from "./Components/HTML COMPO/header.tsx";
+import Profile from "./Components/HTML COMPO/Profile.tsx";
+import Reports from "./Components/HTML COMPO/Report .tsx";
+import Video from "./Components/HTML COMPO/Vid.tsx";
+import Works from "./Components/HTML COMPO/Works.tsx";
+import About from "./Components/HTML COMPO/About.tsx";
+import TopStudents from "./Components/HTML COMPO/TopStudents.tsx";
+import Courses from "./Components/HTML COMPO/Courses.tsx";
+import Pricing from "./Components/HTML COMPO/Pricing.tsx";
+import Blog from "./Components/HTML COMPO/Blog.tsx";
+import Contact from "./Components/HTML COMPO/Contact.tsx";
+import Login from "./Components/HTML COMPO/Login.tsx";
+import SignUp from "./Components/HTML COMPO/SignUp.tsx";
+import Footer from "./Components/HTML COMPO/Footer.tsx";
 
 const App = () => {
   return (
